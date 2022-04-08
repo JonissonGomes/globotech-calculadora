d14219c20e328613c3e92740a4fbabd2bd5cbb1d
JonissonGomes <joni_gomes@hotmail.com>
2022-04-07 22:57:50 -0300

    Inserindo página [Calculadora a rota principal]

--- calculadora/src/App.js
+++ calculadora/src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import { Calculator } from '../src/pages/calculator'
 
 function App() {
   return (
     <div className="App">
-     {/* Front end */}
+     <Calculator />
     </div>
   );
 }
